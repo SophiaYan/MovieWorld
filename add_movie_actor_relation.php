c0e2bae0f0f36059c207e5080e64161180db11d5
shaoc2 <chen.shao@vanderbilt.edu>
2017-10-26 16:40:46 -0700

    add Movie Actor/Director relation

--- add_movie_actor_relation.php
+++ add_movie_actor_relation.php
@@ -7,6 +7,9 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+	div.alert {position: fixed; bottom: 0; right: 0; width: 300px;}
+  </style>
 </head>
 
 <body>
@@ -47,84 +50,123 @@
   	</div>
 </nav>
 
-<form method="POST" action="http://localhost:1438/~cs143/add_movie_info.php">
+<form method="POST" action="add_movie_actor_relation.php">
 	<div class="container-fluid" style="margin-left: 20px">
 		<div class="page-header">
 		  	<h3>Add Movie Actor Relation Section</h3>
 		</div>
 
 		<div class="form-group">
-		  <label for="movie_title">Movie title:</label>
-		  <select class="form-control" id="movie_title" style="width: 400px">
-		    <option>PG-13</option>
-		    <option>R</option>
-		    <option>PG</option>
-		    <option>NC-17</option>
-		    <option>surrendere</option>
-		    <option>G</option>
-		  </select>
+		  	<label>Movie Title:</label>
+		  	<input type="text" class="form-control" name="movie" style="width: 400px" maxlength="100">
 		</div>
 
 		<div class="form-group">
-		  <label for="movie_title">Actor:</label>
-		  <select class="form-control" id="actor" style="width: 400px">
-		    <option>PG-13</option>
-		    <option>R</option>
-		    <option>PG</option>
-		    <option>NC-17</option>
-		    <option>surrendere</option>
-		    <option>G</option>
-		  </select>
+		  	<label>Actor First Name:</label>
+		  	<input type="text" class="form-control" name="actor_first" style="width: 400px" maxlength="100">
 		</div>
 
-		<button type="button" class="btn btn-default">Add Movie Actor Relation</button>
+		<div class="form-group">
+		  	<label>Actor Last Name:</label>
+		  	<input type="text" class="form-control" name="actor_last" style="width: 400px" maxlength="100">
+		</div>
+
+		<div class="form-group">
+		  	<label>Role:</label>
+		  	<input type="text" class="form-control" name="role" style="width: 400px" maxlength="100">
+		</div>
+		<button type="submit" class="btn btn-default">Add Movie Actor Relation</button>
 
 	</div>
 </form>
 
 
 <?php
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	$movie = $_POST["movie"];
+	$actor_first = $_POST["actor_first"];
+	$actor_last = $_POST["actor_last"];
+	$role = $_POST["role"];
 
-$date = DateTime::createFromFormat("d-m-Y", $_POST['date'])->format('Y-m-d');
+	$empty_field = array();
+	if($movie == null){
+		array_push($empty_field, "Movie Title");
+	}
 
-$query = $_POST["query"];
-if($query) {
+	if($actor_first == null) {
+		array_push($empty_field, "Actor First Name");
+	}
+
+	if($actor_last == null) {
+		array_push($empty_field, "Actor Last Name");
+	}
+
+	if($role == null) {
+		array_push($empty_field, "Role");
+	}
+
+	if(sizeof($empty_field) > 0){
+		$empty_field_list;
+		foreach ($empty_field as $key => $value) {
+			$empty_field_list.= $value." is empty <br>";
+		}
+		echo "<div class=\"alert alert-danger\" role=\"alert\"> 
+		<button type=\"button\" class=\"close\" data-dismiss=\"alert\"
+		aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>
+		<strong>Invalid Input!<br></strong>".$empty_field_list."
+		</div>";
+		exit;
+	}
 
 	$db_connection = mysql_connect("localhost", "cs143", "");
-	if(!$db_connection) {
-		$errmsg = mysql_error($db_connection);
-		print "Connection failed: '$errmsg' <br />";
-		exit(1);
+	if(!$db_connection){
+		die('Could not connect: '.mysql_error());
 	}
-
 	mysql_select_db("CS143", $db_connection);
-	$query_to_issue = mysql_real_escape_string($query);
 
-	$rs = mysql_query($query_to_issue, $db_connection);
-	if ($rs) {
-		$column_num = mysql_num_fields($rs);
-		
-		echo "<h3> Result from MySQL: </h3>";
-		echo "<table border=\"1\" cellspacing=\"1\" cellpadding=\"2\">";
-		for ($i = 0; $i < $column_num; $i++) {
-			$column_name = mysql_field_name($rs, $i);
-			echo "<td> $column_name </td>";
-		}
+	$movie_query = sprintf("SELECT id FROM Movie WHERE title = '%s'", mysql_real_escape_string($movie));
+	$movie_query_result = mysql_query($movie_query, $db_connection);
 
-		while($row = mysql_fetch_row($rs)) {
-			echo "<tr>";
-			for ($i = 0; $i < $column_num; $i++) {
-				echo "<td> $row[$i] </td>";
-			}
-			echo "</tr>";
-		}
-		echo "</table>";
+	$actor_query = sprintf("SELECT id FROM Actor WHERE first = '%s' and last = '%s'", 
+		mysql_real_escape_string($actor_first), mysql_real_escape_string($actor_last));
+	$actor_query_result = mysql_query($actor_query, $db_connection);
 
-	} else {
-		echo "There is no matched record in our database. ";
+
+	if(mysql_num_rows($movie_query_result) == 0){
+		echo "<div class=\"alert alert-danger\" role=\"alert\"> 
+		<button type=\"button\" class=\"close\" data-dismiss=\"alert\"
+		aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>
+		<strong>Sorry, no movie matches the title you entered</strong></div>";
+		exit;
+	}else{
+		$mid = mysql_fetch_assoc($movie_query_result)["id"];
 	}
-	mysql_close($db_connection);
 
+	if(mysql_num_rows($actor_query_result) == 0){
+		echo "<div class=\"alert alert-danger\" role=\"alert\"> 
+		<button type=\"button\" class=\"close\" data-dismiss=\"alert\"
+		aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>
+		<strong>Sorry, no Actor/Actress matches the name you entered</strong></div>";
+		exit;
+	}else{
+		$aid = mysql_fetch_assoc($actor_query_result)["id"];
+	}
+
+	$movie_actor_insertion = sprintf("INSERT INTO MovieActor VALUES (%s, %s, '%s')", $mid, $aid, $role);
+
+	if(mysql_query($movie_actor_insertion, $db_connection)){
+		echo "<div class=\"alert alert-success\" role=\"alert\"> 
+		<button type=\"button\" class=\"close\" data-dismiss=\"alert\"
+		aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>
+		<strong>New record inserted successfully</strong></div>";
+	}else{
+		echo "<div class=\"alert alert-danger\" role=\"alert\"> 
+		<button type=\"button\" class=\"close\" data-dismiss=\"alert\"
+		aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>
+		<strong>New record insertion failed</strong></div>";
+	}
+
+	mysql_close($db_connection);
 }
 ?>
 
